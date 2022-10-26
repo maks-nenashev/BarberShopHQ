f886324269841ef87d6f453db39bd299de4f15e8
maksym-nenashev <nenashev@yahoo.com>
2022-10-26 20:45:04 +0200

    dobawil susznost

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:barbershop.db" #Sozdanie bazy dannyh
+
+class Client < ActiveRecord::Base    #Sozdanie susznosti
+ end
 
 get '/' do
 	erb "Hello"
