198e31bc16ebe0a249ee69d53f107c6e5b395a4b
maksym-nenashev <nenashev@yahoo.com>
2022-12-05 00:30:43 +0100

    barber

--- db/migrate/20221204201527_create_barbers.rb
+++ db/migrate/20221204201527_create_barbers.rb
@@ -1,4 +1,17 @@
+
 class CreateBarbers < ActiveRecord::Migration[7.0]
   def change
+  
+  create_table :barbers do |t|
+    t.text :name
+         # W tablice "client" budet sozdany t
+    
+    t.timestamps# ((created_at vs updated_at)) "Sozdanie stolbca s datoy sozdanija i datoy wpisa"
   end
-end
+   
+   Barber.create :name => 'Jessy Pinknam'
+   Barber.create :name => 'Walter White'
+   Barber.create :name => 'Gus Fring'   
+   
+   end
+ end
