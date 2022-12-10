d2d08d380b482e26c778780b10d685a89d12b83c
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2022-12-10 11:52:27 +0100

    dobawil komint

--- db/migrate/20221027060237_create_clients.rb
+++ db/migrate/20221027060237_create_clients.rb
@@ -3,13 +3,13 @@ class CreateClients < ActiveRecord::Migration[7.0]
   def change
   
     create_table :clients do |t|
-    t.text :name
-    t.text :phone               # W tablice "client" budet sozdany t
-    t.text :datestamp
-    t.text :barber
-    t.text :color
+      t.text :name
+      t.text :phone               # W tablice "client" budet sozdany t
+      t.text :datestamp
+      t.text :barber
+      t.text :color
 
-    t.timestamps # ((created_at vs updated_at)) "Sozdanie stolbca s datoy sozdanija datoy wpisa"
+      t.timestamps # ((created_at vs updated_at)) "Sozdanie stolbca s datoy sozdanija datoy wpisa"
   end
  end
 end
